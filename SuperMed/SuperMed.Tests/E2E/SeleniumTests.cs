090aa8544f1e289699322b1a3a4f86685d5e191b
adriansuder <adrian.suder@microsoft.wsei.edu.pl>
2020-01-19 22:44:57 +0100

    add first selenium test

---
+++ SuperMed/SuperMed.Tests/E2E/SeleniumTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+
+
+namespace SuperMed.Tests
+{
+    public class SeleniumTests
+    {
+        [TestFixture]
+        public class FirstTests
+        {
+            IWebDriver _driver;
+            [SetUp]
+            public void StartBrowser()
+            {
+                _driver = new ChromeDriver(@"C:\Users\adria\Desktop\REPOS\SuperMed\chromedriver_win32");
+            }
+
+            [Test]
+            public void RegisterPatientTest()
+            {
+                Random rnd = new Random();
+                var text = Guid.NewGuid().ToString();
+                var date = "01.01.1990";
+                var number = rnd.Next(500000000, 999999999).ToString();
+
+                _driver.Navigate().GoToUrl("https://localhost:44324");
+                _driver.FindElement(By.Id("registerPatient")).Click();
+                _driver.FindElement(By.Id("patientNameInput")).SendKeys(text);
+                _driver.FindElement(By.Id("patientPasswordInput")).SendKeys("!Supermed123");
+                _driver.FindElement(By.Id("patientFirstNameInput")).SendKeys(text);
+                _driver.FindElement(By.Id("patientLastNameInput")).SendKeys(text);
+                _driver.FindElement(By.Id("patientMailInput")).SendKeys(text+"@mail.pl");
+                _driver.FindElement(By.Id("patientPhoneInput")).SendKeys(number);
+                _driver.FindElement(By.Id("patientBirthdayInput")).SendKeys(date);
+                SelectElement select = new SelectElement(_driver.FindElement(By.Id("patientGenderSelect")));
+                select.SelectByText("mężczyzna");
+
+                IWebElement elementRegister = _driver.FindElement(By.ClassName("patientRegisterSubmit"));
+                Actions actions = new Actions(_driver);
+                actions.MoveToElement(elementRegister);
+                _driver.FindElement(By.ClassName("patientRegisterSubmit")).Click();
+                System.Threading.Thread.Sleep(1000);
+
+                _driver.FindElement(By.Id("openPatientsPanel")).Click();
+
+                StringAssert.StartsWith("https://localhost:44324/Patients", _driver.Url);
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            [TearDown]
+            public void CloseBrowser()
+            {
+                _driver.Close();
+            }
+        }
+    }
+}
+
---
+++ SuperMed/SuperMed/Migrations/20200118104845_after_changes.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperMed.Migrations
+{
+    public partial class after_changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adresses");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adresses",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    DoctorId = table.Column<int>(nullable: true),
+                    PatientId = table.Column<int>(nullable: true),
+                    PostalCode = table.Column<string>(nullable: true),
+                    PropertyNumber = table.Column<int>(nullable: true),
+                    StreetNumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adresses", x => x.AddressId);
+                    table.ForeignKey(
+                        name: "FK_Adresses_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "DoctorId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Adresses_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "PatientId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adresses_DoctorId",
+                table: "Adresses",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adresses_PatientId",
+                table: "Adresses",
+                column: "PatientId");
+        }
+    }
+}
